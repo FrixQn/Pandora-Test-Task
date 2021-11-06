f0c7ccbab5d6d8999588c189a6aff154a40ca39a
FrixQn <firxqn@gmail.com>
2021-11-06 16:05:15 +0300

    Summary

--- Pandora Test Task/Assets/Animations.cs
+++ Pandora Test Task/Assets/Animations.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 public class Animations : MonoBehaviour
 {
     private Animator PlayerAnimator {get => GetComponent<Animator>();}
-    [SerializeField] private AnimationClip[] _Animation = new AnimationClip[7];
+    [SerializeField] private AnimationClip[] _Animation = new AnimationClip[4];
+    [SerializeField, Range(0f, 10f)] private float WalkSpeed;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
             PlayerAnimator.Play(_Animation[0].name);
+        }
         if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
             PlayerAnimator.Play(_Animation[1].name);
-        if (Input.GetKeyDown(KeyCode.Keypad2))
+        }
+        if (Input.GetKey(KeyCode.Keypad2))
+        {
             PlayerAnimator.Play(_Animation[2].name);
+            transform.position += Vector3.forward * Time.deltaTime * WalkSpeed;
+        }
         if (Input.GetKeyDown(KeyCode.Keypad3))
-            PlayerAnimator.Play(_Animation[4].name);
-        if (Input.GetKeyDown(KeyCode.Keypad4))
-            PlayerAnimator.Play(_Animation[5].name);
-        if (Input.GetKeyDown(KeyCode.Keypad4))
-            PlayerAnimator.Play(_Animation[6].name);
-        if (Input.GetKeyDown(KeyCode.Keypad7))
-            PlayerAnimator.Play(_Animation[7].name);
-
+        {
+            PlayerAnimator.Play(_Animation[3].name);
+        }
     }
 }
